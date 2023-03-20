6df0376fce52a23e9c08f498f95778d838be4ed1
bububuu <albusor@gmail.com>
2023-03-20 11:21:07 +0100

    Initial commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCp2AlejandroBustamanteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaCp2AlejandroBustamanteApplication.class, args);
+    }
+
+}
